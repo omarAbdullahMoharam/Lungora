650ab709683b76e6e2cea9bda3f096d1ebdd731d
Omar Abdullah <omarmoharam790@gmail.com>
2025-02-15 00:23:33 +0200

    Refactor CustomTextFormField to use StatefulWidget for password visibility toggle

--- lib/features/auth/Presentation/widgets/custom_text_form_field.dart
+++ lib/features/auth/Presentation/widgets/custom_text_form_field.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lungora/core/constants.dart';
 
-// ignore: must_be_immutable
-class CustomTextFormField extends StatelessWidget {
-  CustomTextFormField({
+class CustomTextFormField extends StatefulWidget {
+  const CustomTextFormField({
     super.key,
     required this.labelText,
     required this.isPassword,
@@ -18,27 +17,42 @@ class CustomTextFormField extends StatelessWidget {
   final String hintText;
   // final TextEditingController controller;
   final IconData prefixIcon;
+
+  @override
+  State<CustomTextFormField> createState() => _CustomTextFormFieldState();
+}
+
+class _CustomTextFormFieldState extends State<CustomTextFormField> {
+  bool obscureText = true;
+
   IconData? suffixIcon;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       // controller: controller,
-      obscureText: isPassword,
+      obscureText: widget.isPassword ? obscureText : false,
 
       decoration: InputDecoration(
         prefixIcon: Icon(
-          prefixIcon,
+          widget.prefixIcon,
           color: kPrimaryColor,
         ),
-        hintText: hintText,
-        suffixIcon: suffixIcon != null
-            ? Icon(
-                suffixIcon,
-                color: kPrimaryColor,
+        hintText: widget.hintText,
+        suffixIcon: widget.isPassword
+            ? IconButton(
+                onPressed: () {
+                  setState(() {
+                    obscureText = !obscureText;
+                  });
+                },
+                icon: Icon(
+                  obscureText ? Icons.visibility : Icons.visibility_off,
+                  color: kPrimaryColor,
+                ),
               )
             : null,
-        labelText: labelText,
+        labelText: widget.labelText,
         border: OutlineInputBorder(
           borderSide: BorderSide(color: kPrimaryColor),
           borderRadius: BorderRadius.circular(10.w),
