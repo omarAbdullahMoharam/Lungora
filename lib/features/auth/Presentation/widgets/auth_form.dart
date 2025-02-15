61518c1be231afc36d3de8f6f44a4d29a39f5c47
Omar Abdullah <omarmoharam790@gmail.com>
2025-02-15 19:57:03 +0200

    Refactor AuthForm to use StatefulWidget and add 'Remember me' functionality

--- lib/features/auth/Presentation/widgets/auth_form.dart
+++ lib/features/auth/Presentation/widgets/auth_form.dart
@@ -4,7 +4,7 @@ import 'package:lungora/core/constants.dart';
 import 'package:lungora/features/auth/Presentation/widgets/custom_text_form_field.dart';
 import 'package:lungora/features/auth/Presentation/widgets/social_auth_section.dart';
 
-class AuthForm extends StatelessWidget {
+class AuthForm extends StatefulWidget {
   const AuthForm({
     super.key,
     required this.isLogin,
@@ -13,12 +13,18 @@ class AuthForm extends StatelessWidget {
   final bool isLogin;
 
   @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  bool rememberMe = false;
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 23.w),
       child: Column(
         children: <Widget>[
-          if (!isLogin) ...[
+          if (!widget.isLogin) ...[
             CustomTextFormField(
               labelText: 'Username',
               isPassword: false,
@@ -41,7 +47,66 @@ class AuthForm extends StatelessWidget {
             hintText: 'Password',
             suffixIcon: Icons.remove_red_eye_rounded,
           ),
-          SizedBox(height: 20.h),
+          if (!widget.isLogin) ...[
+            SizedBox(height: 16.h),
+            CustomTextFormField(
+              labelText: 'Confirm Password',
+              isPassword: true,
+              prefixIcon: Icons.lock,
+              hintText: 'Confirm Password',
+              suffixIcon: Icons.remove_red_eye_rounded,
+            ),
+            SizedBox(height: 24.h),
+          ],
+          SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+          if (widget.isLogin) ...[
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.w),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Checkbox(
+                    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    splashRadius: 16.r,
+                    value: rememberMe,
+                    activeColor:
+                        rememberMe ? kPrimaryColor : Colors.grey.shade400,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        rememberMe = !rememberMe;
+                      });
+                    },
+                    checkColor: Colors.white,
+                  ),
+                  Text(
+                    'Remember me',
+                    style: TextStyle(
+                      color: Colors.grey.shade600,
+                      fontSize: 12.sp,
+                    ),
+                  ),
+                  Spacer(),
+                  TextButton(
+                    style: ButtonStyle(
+                      padding: WidgetStateProperty.all<EdgeInsetsGeometry>(
+                        EdgeInsets.zero,
+                      ),
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      'Forgot Password?',
+                      style: TextStyle(
+                        color: Colors.black.withValues(alpha: 0.8),
+                        fontSize: 12.sp,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+          ],
           ElevatedButton(
             onPressed: () {},
             style: ElevatedButton.styleFrom(
@@ -52,11 +117,11 @@ class AuthForm extends StatelessWidget {
               backgroundColor: kPrimaryColor,
             ),
             child: Text(
-              isLogin ? 'Login' : 'Register',
+              widget.isLogin ? 'Login' : 'Register',
               style: const TextStyle(color: Colors.white),
             ),
           ),
-          SizedBox(height: 42.h),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.02),
           Row(
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
@@ -65,7 +130,7 @@ class AuthForm extends StatelessWidget {
               Expanded(
                 child: Divider(
                   color: Colors.black.withValues(alpha: 0.5),
-                  thickness: 2,
+                  thickness: 1,
                   height: 10.h,
                   indent: 10.w,
                   endIndent: 10.w,
@@ -73,7 +138,7 @@ class AuthForm extends StatelessWidget {
               ),
             ],
           ),
-          SizedBox(height: 30.h),
+          SizedBox(height: 20.h),
           SocialAuthSection(),
           SizedBox(height: 24.h),
         ],
