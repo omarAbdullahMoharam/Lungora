e73be128c4a6ced86ae6dc8f4ddbfa0aeb099a71
Omar Abdullah <omarmoharam790@gmail.com>
2025-02-18 16:40:47 +0200

    Implement authentication repository and integrate dependency injection for API services

--- lib/core/utils/api_services.dart
+++ lib/core/utils/api_services.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:dio/dio.dart';
 import 'package:lungora/features/Auth/data/models/auth_response_model.dart';
 import 'package:retrofit/retrofit.dart';
@@ -5,7 +7,7 @@ part 'api_services.g.dart';
 
 @RestApi(baseUrl: "https://lungora.runasp.net/")
 abstract class ApiServices {
-  factory ApiServices(Dio dio, {String baseUrl}) = _ApiServices;
+  factory ApiServices(Dio dio) = _ApiServices;
   @POST("api/Auth/login")
   Future<AuthResponse> loginUser(@Body() Map<String, dynamic> body);
   @POST("api/Auth/Register")
