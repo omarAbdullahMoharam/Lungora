c46fea2008ea64440ae5cb8a8817d5a00ed58618
Omar Abdullah <omarmoharam790@gmail.com>
2025-03-03 00:54:39 +0200

    Add InitialEmptyChat widget and ChatInputField for improved chat interface

--- lib/features/Chat/presentation/view/widgets/chat_screen.dart
+++ lib/features/Chat/presentation/view/widgets/chat_screen.dart
@@ -1,9 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:lottie/lottie.dart';
-import 'package:lungora/core/constants.dart';
 import 'package:lungora/core/utils/styles.dart';
+import 'package:lungora/features/Chat/presentation/view/widgets/custom_input_field.dart';
+import 'package:lungora/features/Chat/presentation/view/widgets/initial_empty_chat.dart';
 import 'package:lungora/features/Chat/presentation/view/widgets/loading_message.dart';
 import 'package:lungora/features/Chat/presentation/view/widgets/message_bubble.dart';
 import 'package:lungora/features/Chat/presentation/view_model/chat_cubit/chat_cubit.dart';
@@ -31,154 +32,57 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('AI Chat Bot'),
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 16.0),
+          child: IconButton(
+            icon: Icon(CupertinoIcons.back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        centerTitle: true,
+        title: Text(
+          'Chat with Ai',
+          style: Styles.textStyle24,
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0),
+            child: Container(),
+          )
+        ],
       ),
       body: BlocBuilder<ChatCubit, ChatState>(
         builder: (context, state) {
-          return Column(
-            children: [
-              Expanded(
-                child: state.messages.isEmpty
-                    ? InitialEmptyChat()
-                    : ListView(
-                        padding: EdgeInsets.symmetric(horizontal: 16.w),
-                        children: [
-                          // Show message bubbles
-                          ...state.messages.map(
-                              (message) => ChatMessageBubble(message: message)),
+          return Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.w),
+            child: Column(
+              children: [
+                Expanded(
+                  child: state.messages.isEmpty
+                      ? InitialEmptyChat()
+                      : ListView(
+                          children: [
+                            // Show message bubbles
+                            ...state.messages.map((message) =>
+                                ChatMessageBubble(message: message)),
 
-                          // Show typing indicator if needed
-                          if (state.isTyping) const LoadingMessageIndicator(),
-                        ],
-                      ),
-              ),
-              Container(
-                margin: EdgeInsets.only(
-                  right: 16.w,
-                  left: 16.w,
-                  top: 6.h,
-                  bottom: 16.h,
-                ),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(24),
-                    color: Colors.white,
-                    border: Border(top: BorderSide(color: Colors.grey[300]!)),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black12,
-                        blurRadius: 4,
-                        offset: Offset(0, 2),
-                      ),
-                    ]),
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    SizedBox(
-                      height: 48.h,
-                      width: MediaQuery.of(context).size.width * 0.65,
-                      child: Scrollbar(
-                        controller: _scrollController,
-                        scrollbarOrientation: ScrollbarOrientation.right,
-                        trackVisibility: true,
-                        child: TextField(
-                          enableSuggestions: true,
-                          autocorrect: true,
-                          onChanged: (value) {
-                            setState(() {
-                              _promptController.text = value;
-                            });
-                            _scrollController.jumpTo(
-                              _scrollController.position.maxScrollExtent,
-                            );
-                          },
-                          scrollController: _scrollController,
-                          maxLines: null,
-                          minLines: 1,
-                          enabled: !state.isLoading,
-                          controller: _promptController,
-                          decoration: InputDecoration(
-                            hintText: 'Type your message...',
-                            hintStyle: Styles.textStyle12,
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(24),
-                              borderSide: BorderSide.none,
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(24),
-                              borderSide: BorderSide.none,
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(24),
-                              borderSide: BorderSide.none,
-                            ),
-                            contentPadding: const EdgeInsets.symmetric(
-                                horizontal: 16, vertical: 12),
-                          ),
+                            // Show typing indicator if needed
+                            if (state.isTyping) const LoadingMessageIndicator(),
+                          ],
                         ),
-                      ),
-                    ),
-                    Spacer(),
-                    IconButton(
-                      icon: const Icon(Icons.send),
-                      onPressed: () {
-                        if (_promptController.text.isNotEmpty) {
-                          context
-                              .read<ChatCubit>()
-                              .sendMessage(_promptController.text);
-                          _promptController.clear();
-                        }
-                        FocusScope.of(context).unfocus();
-                        _scrollController.jumpTo(
-                          _scrollController.position.maxScrollExtent,
-                        );
-                        _scrollController.animateTo(
-                          _scrollController.position.maxScrollExtent,
-                          duration: Duration(milliseconds: 300),
-                          curve: Curves.easeOut,
-                        );
-                        setState(() {
-                          _promptController.clear();
-                        });
-                      },
-                      style: state.isLoading || _promptController.text.isEmpty
-                          ? null
-                          : IconButton.styleFrom(
-                              padding: EdgeInsets.zero,
-                              backgroundColor: kSecondaryColor,
-                              foregroundColor: Colors.white,
-                            ),
-                    ),
-                  ],
                 ),
-              ),
-            ],
+                ChatInputField(
+                  promptController: _promptController,
+                  scrollController: _scrollController,
+                  isLoading: state.isLoading,
+                ),
+              ],
+            ),
           );
         },
       ),
-    );
-  }
-}
-
-class InitialEmptyChat extends StatelessWidget {
-  const InitialEmptyChat({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Lottie.asset(
-          "assets/images/animatedRobot.json",
-          width: 300,
-          height: 200,
-        ),
-        Text(
-          'Feel free to ask what you want 🥰',
-          style: Styles.textStyle20,
-        ),
-      ],
     );
   }
 }
---
+++ lib/features/Chat/presentation/view/widgets/custom_input_field.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:lungora/core/constants.dart';
+import 'package:lungora/core/utils/styles.dart';
+import 'package:lungora/features/Chat/presentation/view_model/chat_cubit/chat_cubit.dart';
+
+class ChatInputField extends StatefulWidget {
+  final TextEditingController promptController;
+  final ScrollController scrollController;
+  final bool isLoading;
+
+  const ChatInputField({
+    super.key,
+    required this.promptController,
+    required this.scrollController,
+    required this.isLoading,
+  });
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _ChatInputFieldState createState() => _ChatInputFieldState();
+}
+
+class _ChatInputFieldState extends State<ChatInputField> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        top: 6.h,
+        bottom: 16.h,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(24),
+        color: Colors.white,
+        border: Border(top: BorderSide(color: Colors.grey[300]!)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 4,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          SizedBox(
+            height: 48.h,
+            width: MediaQuery.of(context).size.width * 0.65,
+            child: Scrollbar(
+              controller: widget.scrollController,
+              scrollbarOrientation: ScrollbarOrientation.right,
+              trackVisibility: true,
+              child: TextField(
+                enableSuggestions: true,
+                autocorrect: true,
+                onChanged: (value) {
+                  setState(() {
+                    widget.promptController.text = value;
+                  });
+                  widget.scrollController.jumpTo(
+                    widget.scrollController.position.maxScrollExtent,
+                  );
+                },
+                scrollController: widget.scrollController,
+                maxLines: null,
+                minLines: 1,
+                enabled: !widget.isLoading,
+                controller: widget.promptController,
+                decoration: InputDecoration(
+                  hintText: 'Type your message...',
+                  hintStyle: Styles.textStyle12,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(24),
+                    borderSide: BorderSide.none,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(24),
+                    borderSide: BorderSide.none,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(24),
+                    borderSide: BorderSide.none,
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 8,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Spacer(),
+          IconButton(
+            icon: const Icon(Icons.send),
+            onPressed: () {
+              if (widget.promptController.text.isNotEmpty) {
+                context
+                    .read<ChatCubit>()
+                    .sendMessage(widget.promptController.text);
+                widget.promptController.clear();
+              }
+              FocusScope.of(context).unfocus();
+              widget.scrollController.jumpTo(
+                widget.scrollController.position.maxScrollExtent,
+              );
+              widget.scrollController.animateTo(
+                widget.scrollController.position.maxScrollExtent,
+                duration: Duration(milliseconds: 300),
+                curve: Curves.easeOut,
+              );
+              setState(() {
+                widget.promptController.clear();
+              });
+            },
+            style: widget.isLoading || widget.promptController.text.isEmpty
+                ? null
+                : IconButton.styleFrom(
+                    padding: EdgeInsets.zero,
+                    backgroundColor: kSecondaryColor,
+                    foregroundColor: Colors.white,
+                  ),
+          ),
+        ],
+      ),
+    );
+  }
+}
