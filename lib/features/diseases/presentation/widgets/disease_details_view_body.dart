02035f17634ca87ff1140dffaf31791872564d63
Your Name <you@example.com>
2025-03-07 17:03:46 +0200

    edit in disease details

--- lib/features/diseases/presentation/widgets/disease_details_view_body.dart
+++ lib/features/diseases/presentation/widgets/disease_details_view_body.dart
@@ -23,116 +23,119 @@ class DiseaseDetailsViewBody extends StatelessWidget {
       padding: EdgeInsets.symmetric(
         horizontal: 24.w,
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(16.w),
-            child: Image.asset(
-              'assets/images/lung_desease.png',
-              height: 160.h,
-              width: double.infinity,
-              fit: BoxFit.cover,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(16.w),
+              child: Image.asset(
+                'assets/images/lung_desease.png',
+                height: 160.h,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          SizedBox(height: 16.h),
-          Center(
-            child: Text(
-              diseaseName,
-              style: Styles.textStyle24.copyWith(
+            SizedBox(height: 16.h),
+            Center(
+              child: Text(
+                diseaseName,
+                style: Styles.textStyle24.copyWith(
+                  fontFamily: 'Inter',
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            DiseaseStatsSection(),
+            SizedBox(height: 24.h),
+            Text(
+              'Description',
+              textAlign: TextAlign.start,
+              style: Styles.textStyleInter16.copyWith(
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            SizedBox(height: 8.h),
+            Text(
+              diseaseDescription,
+              style: Styles.textStyle12.copyWith(
                 fontFamily: 'Inter',
               ),
             ),
-          ),
-          const SizedBox(height: 16),
-          DiseaseStatsSection(),
-          SizedBox(height: 24.h),
-          Text(
-            'Description',
-            textAlign: TextAlign.start,
-            style: Styles.textStyleInter16.copyWith(
-              fontWeight: FontWeight.w700,
-            ),
-          ),
-          SizedBox(height: 8.h),
-          Text(
-            diseaseDescription,
-            style: Styles.textStyle12.copyWith(
-              fontFamily: 'Inter',
-            ),
-          ),
-          SizedBox(height: 24.h),
+            SizedBox(height: 24.h),
 
-          Text(
-            'Treatment methods',
-            style: Styles.textStyleInter16.copyWith(
-              fontWeight: FontWeight.w700,
+            Text(
+              'Treatment methods',
+              style: Styles.textStyleInter16.copyWith(
+                fontWeight: FontWeight.w700,
+              ),
             ),
-          ),
-          Text.rich(
-            TextSpan(
-              children: [
-                TextSpan(
-                  text: treatmentMethods.replaceAll(r'\n', '\n'),
-                  style: Styles.textStyle12.copyWith(
-                    fontFamily: 'Inter',
-                  ),
-                ),
-              ],
-            ),
-          ),
-
-          // Text(
-          //   'Treatment methods',
-          //   textAlign: TextAlign.start,
-          //   style: Styles.textStyleInter16.copyWith(
-          //     fontWeight: FontWeight.w700,
-          //   ),
-          // ),
-          // Text(
-          //   treatmentMethods,
-          //   style: Styles.textStyle12.copyWith(
-          //     fontFamily: 'Inter',
-          //   ),
-          // ),
-
-          const Spacer(),
-          Padding(
-            padding: EdgeInsets.only(bottom: 16.h),
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    context.push(AppRouter.kDoctorView);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: kPrimaryColor,
-                    foregroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16.h),
+            Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(
+                    text: treatmentMethods.replaceAll(r'\n', '\n'),
+                    style: Styles.textStyle12.copyWith(
+                      fontFamily: 'Inter',
                     ),
                   ),
-                  child: const Text("Discuss a doctor"),
-                ),
-                OutlinedButton(
-                  onPressed: () {
-                    context.push(AppRouter.kChatView);
-                  },
-                  style: OutlinedButton.styleFrom(
-                    foregroundColor: kPrimaryColor,
-                    side: const BorderSide(color: kPrimaryColor, width: 2),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16.h),
-                    ),
-                  ),
-                  child: const Text("Chatbot"),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+
+            // Text(
+            //   'Treatment methods',
+            //   textAlign: TextAlign.start,
+            //   style: Styles.textStyleInter16.copyWith(
+            //     fontWeight: FontWeight.w700,
+            //   ),
+            // ),
+            // Text(
+            //   treatmentMethods,
+            //   style: Styles.textStyle12.copyWith(
+            //     fontFamily: 'Inter',
+            //   ),
+            // ),
+
+            SizedBox(height: 24.h),
+
+            Padding(
+              padding: EdgeInsets.only(bottom: 16.h),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      context.push(AppRouter.kDoctorView);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: kPrimaryColor,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16.h),
+                      ),
+                    ),
+                    child: const Text("Discuss a doctor"),
+                  ),
+                  OutlinedButton(
+                    onPressed: () {
+                      context.push(AppRouter.kChatView);
+                    },
+                    style: OutlinedButton.styleFrom(
+                      foregroundColor: kPrimaryColor,
+                      side: const BorderSide(color: kPrimaryColor, width: 2),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16.h),
+                      ),
+                    ),
+                    child: const Text("Chatbot"),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
